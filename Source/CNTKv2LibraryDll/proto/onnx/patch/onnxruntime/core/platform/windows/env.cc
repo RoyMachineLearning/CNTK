20da73490873614da7432edefb8e605b7055959e
Bowen Bao <bowbao@microsoft.com>
2018-12-14 10:16:57 -0800

    Submodule onnxruntime, and remove previous drop.
    * A few patches are required to build cntk_uwp.
    * Use proto from onnxruntime/protobuf instead of from onnx.
    * TODO: Some issues with onnx_op_test RNN and OptimizedRNNStack from shape inference.

--- Source/CNTKv2LibraryDll/proto/onnx/patch/onnxruntime/core/platform/windows/env.cc
+++ Source/CNTKv2LibraryDll/proto/onnx/patch/onnxruntime/core/platform/windows/env.cc
@@ -14,9 +14,6 @@ limitations under the License.
 ==============================================================================*/
 // Portions Copyright (c) Microsoft Corporation
 
-#include <limits>
-static const int std_numeric_limits_int_max = std::numeric_limits<int>::max();
-static const unsigned int std_numeric_limits_DWORD_max = std::numeric_limits<unsigned int>::max();
 #include <Shlwapi.h>
 #include <Windows.h>
 
@@ -28,7 +25,6 @@ static const unsigned int std_numeric_limits_DWORD_max = std::numeric_limits<uns
 
 #include "core/common/logging/logging.h"
 #include "core/platform/env.h"
-
 
 namespace onnxruntime {
 
@@ -46,28 +42,6 @@ class StdThread : public Thread {
 };
 
 class WindowsEnv : public Env {
- private:
-  template <typename T, typename F>
-  static common::Status FileExists_(T fname, F f) {
-    if (!fname)
-      return common::Status(common::ONNXRUNTIME, common::INVALID_ARGUMENT, "file name is nullptr");
-    struct _stat st;
-    int ret = f(fname, &st);
-    if (ret == 0) {
-      if (st.st_mode & _S_IFREG)
-        return common::Status::OK();
-      return ONNXRUNTIME_MAKE_STATUS(ONNXRUNTIME, FAIL, fname, "is not a regular file");
-    }
-    switch (errno) {
-      case ENOENT:
-        return common::Status(common::ONNXRUNTIME, common::NO_SUCHFILE, "");
-      case EINVAL:
-        return common::Status(common::ONNXRUNTIME, common::INVALID_ARGUMENT, "");
-      default:
-        return common::Status(common::ONNXRUNTIME, common::FAIL, "unknown error inside FileExists");
-    }
-  }
-
  public:
   void SleepForMicroseconds(int64_t micros) const override { Sleep(static_cast<DWORD>(micros) / 1000); }
 
@@ -129,6 +103,7 @@ class WindowsEnv : public Env {
   }
 
   common::Status FileOpenWr(const std::wstring& path, /*out*/ int& fd) const override {
+    // TODO: make sure O_TRUNC is added.
     _wsopen_s(&fd, path.c_str(), _O_CREAT | O_TRUNC | _O_SEQUENTIAL | _O_BINARY | _O_WRONLY, _SH_DENYWR, _S_IREAD | _S_IWRITE);
     if (0 > fd) {
       return common::Status(common::SYSTEM, errno);
@@ -145,6 +120,7 @@ class WindowsEnv : public Env {
   }
 
   common::Status FileOpenWr(const std::string& path, /*out*/ int& fd) const override {
+    // TODO: make sure O_TRUNC is added.
     _sopen_s(&fd, path.c_str(), _O_CREAT | O_TRUNC | _O_SEQUENTIAL | _O_BINARY | _O_WRONLY, _SH_DENYWR, _S_IREAD | _S_IWRITE);
     if (0 > fd) {
       return common::Status(common::SYSTEM, errno);
@@ -160,72 +136,13 @@ class WindowsEnv : public Env {
     return Status::OK();
   }
 
-  common::Status FileExists(const char* fname) const override {
-    return FileExists_(fname, _stat);
-  }
-  common::Status FileExists(const wchar_t* fname) const override {
-    return FileExists_(fname, _wstat);
-  }
-  common::Status ReadFileAsString(const char* fname, std::string* out) const override {
-    if (!fname)
-      return common::Status(common::ONNXRUNTIME, common::INVALID_ARGUMENT, "file name is nullptr");
-    size_t flen = strlen(fname);
-    if (flen >= std_numeric_limits_int_max) {
-      return ONNXRUNTIME_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT, "input path too long");
-    }
-    int len = MultiByteToWideChar(CP_ACP, 0, fname, (int)(flen + 1), nullptr, 0);
-    if (len <= 0) {
-      return ONNXRUNTIME_MAKE_STATUS(ONNXRUNTIME, FAIL, "MultiByteToWideChar error");
-    }
-    std::wstring wStreamName((size_t)(len - 1), L'\0');
-    MultiByteToWideChar(CP_ACP, 0, fname, (int)flen, (LPWSTR)wStreamName.data(), len);
-    return ReadFileAsString(wStreamName.c_str(), out);
-  }
-
-  common::Status ReadFileAsString(const wchar_t* fname, std::string* out) const override {
-    //if (!fname)
-    //  return common::Status(common::ONNXRUNTIME, common::INVALID_ARGUMENT, "file name is nullptr");
-    //if (!out) {
-    //  return common::Status(common::ONNXRUNTIME, common::INVALID_ARGUMENT, "'out' cannot be NULL");
-    //}
-    //char errbuf[512];
-    //HANDLE hFile = CreateFileW(fname, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    //if (hFile == INVALID_HANDLE_VALUE) {
-    //  int err = GetLastError();
-    //  _snprintf_s(errbuf, _TRUNCATE, "%s:%d open file %ls fail, errcode = %d", __FILE__, (int)__LINE__, fname, err);
-    //  return common::Status(common::ONNXRUNTIME, common::FAIL, errbuf);
-    //}
-    //LARGE_INTEGER filesize;
-    //if (!GetFileSizeEx(hFile, &filesize)) {
-    //  int err = GetLastError();
-    //  _snprintf_s(errbuf, _TRUNCATE, "%s:%d GetFileSizeEx %ls fail, errcode = %d", __FILE__, (int)__LINE__, fname, err);
-    //  CloseHandle(hFile);
-    //  return common::Status(common::ONNXRUNTIME, common::FAIL, errbuf);
-    //}
-    //out->resize(filesize.QuadPart, '\0');
-    //if (filesize.QuadPart > std::numeric_limits<DWORD>::max()) {
-    //  _snprintf_s(errbuf, _TRUNCATE, "%s:%d READ file %ls fail, file size too long", __FILE__, (int)__LINE__, fname);
-    //  CloseHandle(hFile);
-    //  //we can support that with a while loop
-    //  return common::Status(common::ONNXRUNTIME, common::NOT_IMPLEMENTED, errbuf);
-    //}
-    //if (!ReadFile(hFile, (void*)out->data(), (DWORD)filesize.QuadPart, nullptr, nullptr)) {
-    //  int err = GetLastError();
-    //  _snprintf_s(errbuf, _TRUNCATE, "%s:%d ReadFileEx %ls fail, errcode = %d", __FILE__, (int)__LINE__, fname, err);
-    //  CloseHandle(hFile);
-    //  return common::Status(common::ONNXRUNTIME, common::FAIL, errbuf);
-    //}
-    //CloseHandle(hFile);
-    return common::Status::OK();
-  }
-
-  virtual Status LoadLibrary(const std::string& library_filename, void** handle) const override {
+  virtual Status LoadDynamicLibrary(const std::string& library_filename, void** handle) const override {
     ONNXRUNTIME_UNUSED_PARAMETER(library_filename);
     ONNXRUNTIME_UNUSED_PARAMETER(handle);
     ONNXRUNTIME_NOT_IMPLEMENTED(__FUNCTION__, " is not implemented");
   }
 
-  virtual common::Status UnloadLibrary(void* handle) const override {
+  virtual common::Status UnloadDynamicLibrary(void* handle) const override {
     ONNXRUNTIME_UNUSED_PARAMETER(handle);
     ONNXRUNTIME_NOT_IMPLEMENTED(__FUNCTION__, " is not implemented");
   }
@@ -250,12 +167,14 @@ class WindowsEnv : public Env {
     // versions of Windows. For that reason, we try to look it up in
     // kernel32.dll at runtime and use an alternative option if the function
     // is not available.
-    //HMODULE module = GetModuleHandleW(L"kernel32.dll");
-    //if (module != nullptr) {
-    //  auto func = (FnGetSystemTimePreciseAsFileTime)GetProcAddress(
-    //      module, "GetSystemTimePreciseAsFileTime");
-    //  GetSystemTimePreciseAsFileTime_ = func;
-    //}
+#ifndef IsUWP
+    HMODULE module = GetModuleHandleW(L"kernel32.dll");
+    if (module != nullptr) {
+      auto func = (FnGetSystemTimePreciseAsFileTime)GetProcAddress(
+          module, "GetSystemTimePreciseAsFileTime");
+      GetSystemTimePreciseAsFileTime_ = func;
+    }
+#endif
   }
 
   typedef VOID(WINAPI* FnGetSystemTimePreciseAsFileTime)(LPFILETIME);
@@ -264,7 +183,7 @@ class WindowsEnv : public Env {
 
 }  // namespace
 
-#ifdef _WIN32
+#if defined(PLATFORM_WINDOWS)
 const Env& Env::Default() {
   return WindowsEnv::Instance();
 }
